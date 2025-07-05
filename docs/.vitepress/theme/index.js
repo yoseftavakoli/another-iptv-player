89092614fa17bcad1f2912cbd842495f8077828a
bsogulcan <bsogulcan@gmail.com>
2025-07-06 01:57:05 +0300

    fix(docs): vitepress ssr

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -4,15 +4,16 @@ import './custom.css'
 export default {
     ...DefaultTheme,
     enhanceApp({ app, router, siteData }) {
-        // Varsayılan enhanceApp fonksiyonu varsa çağır
         DefaultTheme.enhanceApp && DefaultTheme.enhanceApp({ app, router, siteData });
 
-        window.openLightbox = function (img) {
-            document.getElementById('lightbox-modal').style.display = 'flex';
-            document.getElementById('lightbox-img').src = img.src;
-        }
-        window.closeLightbox = function () {
-            document.getElementById('lightbox-modal').style.display = 'none';
+        if (typeof window !== 'undefined') {
+            window.openLightbox = function (img) {
+                document.getElementById('lightbox-modal').style.display = 'flex';
+                document.getElementById('lightbox-img').src = img.src;
+            }
+            window.closeLightbox = function () {
+                document.getElementById('lightbox-modal').style.display = 'none';
+            }
         }
     }
 }
